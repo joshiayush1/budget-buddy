8585a9c086baf041e246d29687b55b556ed3f8b0
Ayush Joshi <ayushjoshi207@gmail.com>
2024-10-28 17:58:47 +0530

    added readme

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,17 +3,38 @@ import React from "react";
 const Login = () => {
   return (
     <>
-    <div className="LoginClass w-screen h-screen flex justify-center items-center">
-      <div className="bg-white text-black w-96 h-auto px-6 py-5 flex flex-col border rounded-md">
-        <h1 className="text-lg font-semibold mt-2">Welcome to BudgetBuddy! 💸</h1>
-        <p className="text-sm mb-8 text-gray-500">Login to your profile</p>
-        <label htmlFor="email" className="pb-1 text-sm">Email Address</label>
-        <input type="text" name="email" className="w-full h-10 border rounded-md text-sm mb-6 pl-2 "/>
-        <label htmlFor="name" className="pb-1 text-sm">Password</label>
-        <input type="text" name="password" className="w-full h-10 border rounded-md text-sm mb-6 pl-2 outline-offset-1"/>
-        <button className="w-full h-10 bg-[#1B262C] text-white rounded-md text-sm">Login</button>
+      <div className="LoginClass w-screen h-screen flex justify-center items-center">
+        <div className="bg-white text-black w-96 h-auto px-6 py-5 flex flex-col border rounded-md">
+          <h1 className="text-lg font-semibold mt-2">
+            Welcome to BudgetBuddy! 💸
+          </h1>
+          <p className="text-sm mb-8 text-gray-500">Login to your profile</p>
+          <form action="/signup" method="">
+            <label htmlFor="email" className="pb-1 text-sm">
+              Email Address
+            </label>
+            <input
+              type="text"
+              name="email"
+              className="w-full h-10 border rounded-md text-sm mb-6 pl-2 "
+            />
+            <label htmlFor="name" className="pb-1 text-sm">
+              Password
+            </label>
+            <input
+              type="text"
+              name="password"
+              className="w-full h-10 border rounded-md text-sm mb-6 pl-2 outline-offset-1"
+            />
+            <button className="w-full h-10 bg-[#1B262C] text-white rounded-md text-sm hover:opacity-95">
+              Login
+            </button>
+          </form>
+          <a href="" className="text-xs text-[#1B262C] mt-4">
+            Don't have a profile? Tap here
+          </a>
+        </div>
       </div>
-          </div>
     </>
   );
 };
---
+++ readme.md
@@ -0,0 +1,4 @@
+In real-world applications, this setup of allows for a separation of concerns, where:
+
+Express - handles data processing, storage, and API logic.
+React - manages the user interface and interactions with the data served by Express.
